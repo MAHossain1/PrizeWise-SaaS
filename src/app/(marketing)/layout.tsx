f7b2cce8a055105853873bd477c0aa853b079be2
MAHossain1 <mahpro110@gmail.com>
2024-10-26 18:21:38 +0600

    Project started.

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+export default function MarketingLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+      {/* <Navbar /> */}
+      <h1>addlfskle4wesedklftrfr</h1>
+      {children}
+    </div>
+  );
+}
